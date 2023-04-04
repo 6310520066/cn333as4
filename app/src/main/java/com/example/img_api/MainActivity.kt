9285158e6317265d5a65e16a70725ba85a6e50a4
6310520066 <6310520066@student.tu.ac.th>
2023-04-04 23:56:23 +0700

    Edit Some element

--- app/src/main/java/com/example/img_api/MainActivity.kt
+++ app/src/main/java/com/example/img_api/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
@@ -21,7 +22,12 @@ import coil.request.ImageRequest
 import com.example.img_api.ui.theme.ImgapiTheme
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 
 
 class MainActivity : ComponentActivity() {
@@ -35,48 +41,73 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     var category by remember { mutableStateOf("movie") }
-                    var width by remember { mutableStateOf(150) }
-                    var height by remember { mutableStateOf(220) }
+                    var width by remember { mutableStateOf("150") }
+                    var height by remember { mutableStateOf("220") }
                     var imageUrl by remember { mutableStateOf("https://api.lorem.space/image/movie?w=150&h=220") }
+                    var hint by remember { mutableStateOf("") }
 
-                    Column(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        CategoryDropdown(category = category) {
-                            category = it
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Text("Width")
-                        Slider(
-                            value = width.toFloat(),
-                            onValueChange = { width = it.toInt() },
-                            valueRange = 8f..2000f
-                        )
-                        Text(width.toString())
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Text("Height")
-                        Slider(
-                            value = height.toFloat(),
-                            onValueChange = { height = it.toInt() },
-                            valueRange = 8f..2000f
-                        )
-                        Text(height.toString())
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(
-                            onClick = {
-                                imageUrl = "https://api.lorem.space/image/$category?w=$width&h=$height"
-                            }
-                        ) {
-                            Text("Display Image")
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Box(
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White))
+                    {
+                        Column(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .height(400.dp)
-                                .background(Color.White)
+                                .padding(16.dp)
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
                         ) {
-                            DisplayImage(url = imageUrl, modifier = Modifier.fillMaxSize())
+                            CategoryDropdown(category = category) {
+                                category = it
+                            }
+                            Spacer(modifier = Modifier.height(16.dp))
+                            TextField(
+                                value = width,
+                                onValueChange = { width = it },
+                                label = { Text("Width") },
+                                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            TextField(
+                                value = height,
+                                onValueChange = { height = it },
+                                label = { Text("Height") },
+                                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Button(
+                                onClick = {
+                                    val w = width.toIntOrNull()
+                                    val h = height.toIntOrNull()
+                                    if (w == null || h == null || w !in 8..2000 || h !in 8..2000) {
+                                        hint = "Hint: Width and Height are between 8 and 2000."
+                                        imageUrl = ""
+                                    } else {
+                                        imageUrl = "https://api.lorem.space/image/$category?w=$w&h=$h"
+                                        hint = ""
+                                    }
+                                }
+                            ) {
+                                Text("Display Image")
+                            }
+                            if (hint.isNotEmpty()) {
+                                Text(
+                                    hint,
+                                    color = MaterialTheme.colors.secondary
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(400.dp)
+                                    .background(Color.White)
+                            ) {
+                                if (imageUrl.isNotEmpty()) {
+                                    DisplayImage(url = imageUrl, modifier = Modifier.fillMaxSize())
+                                }
+                    }
+
                         }
                     }
                 }
@@ -86,20 +117,22 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Composable
 fun DisplayImage(
     url: String,
     modifier: Modifier = Modifier
 ) {
-    val imageUrl = "$url"
     Box(modifier = modifier) {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
-                .data(imageUrl)
+                .data(url)
                 .crossfade(true)
                 .build(),
             contentDescription = "",
-            contentScale = ContentScale.FillBounds
+            contentScale = ContentScale.FillBounds,
+            placeholder = painterResource(R.drawable.loading_img),
+            modifier = Modifier.align(Alignment.Center)
         )
     }
 }
@@ -112,15 +145,28 @@ fun CategoryDropdown(
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    Text("Category")
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically)
+    {
+        Text("Category: ", fontWeight = FontWeight.Medium)
+        Text(
+                category,
+        modifier = Modifier.clickable { expanded = true }
+        )
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Expand category dropdown"
+            )
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxWidth(),
         contentAlignment = Alignment.CenterStart
     ) {
-        Text(
-            category,
-            modifier = Modifier.clickable { expanded = true }
-        )
+
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
